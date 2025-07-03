8a5bd5484b68cf610908809b1ac3bcff62d0286f
AleGomez <4le.goo@gmail.com>
2025-07-02 21:17:37 -0600

    UPDATE index.js

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const itemsContainer = document.querySelector("#list-items")
-itemsContainer.className = "row row-cols-1 row-cols-sm2 row-cols-md-3 g-3"
-const urlApi = "https://rickandmortyapi.com/api/character"
+function htmlConstructor(){
+  const container = document.querySelector("#container")
+  container.className = "container-fluid mx-2"
 
+  const header = document.querySelector("#header")
+  header.className = ""
+
+  const titlePage = document.querySelector("#title")
+  titlePage.className = "text-primary"
+  titlePage.innerText = "The Rick and Morty API"
+}
+const itemsContainer = document.querySelector("#list-items")
+itemsContainer.className = "row mx-2"
+
+const urlApi = "https://rickandmortyapi.com/api/character"
 
 function addItem(item){
 
     const charCard = document.createElement("section")
-    charCard.className = "card mx-.5 my-2"
+    charCard.className = "card mx-2 my-2 p-3"
     charCard.style = "width: 18rem"
     itemsContainer.append(charCard)  
 
@@ -20,8 +31,8 @@ function addItem(item){
     charCardBody.append(charCardTitle)
 
     const charCardStatus = document.createElement("p")
-    charCardStatus.className = "card-text"
-    charCardStatus.innerText = item.status
+    charCardStatus.className = "card-text p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3"
+    charCardStatus.innerText = "Status: "+ item.status
     charCardBody.append(charCardStatus)
 
     const charCardImg = document.createElement("img")
@@ -44,6 +55,12 @@ function fetchCharacter(){
   })
   .then(data => {
     console.log(data); // Procesa los datos obtenidos - Process the fetched data
+    //agregar la respuesta al localstorage
+    // Convertir JSON en cadena
+    const dataString = JSON.stringify(data.results);
+    localStorage.setItem('characters',dataString);
+    console.log('Characters guardados en storage')
+
     //iteracion
     data.results.forEach(name => {
       addItem(name)
@@ -55,6 +72,35 @@ function fetchCharacter(){
 
 }
 
-fetchCharacter()
+
+function loadCharFromStorage (){
+  const storedChar = localStorage.getItem('characters');
+
+  if (storedChar){
+    try{
+      const charArray = JSON.parse(storedChar);
+      console.log('Loading characters from localStorage:', charArray);
+      charArray.forEach(name => {
+      addItem(name)
+      })
+      return true;//Indica que se cargaron los datos del localstorage
+    }catch (error){
+      console.log('Error pasando characters del localstorage:', error);
+      return false;
+    } 
+  }else{
+    console.log('No se encontraron los chars en el localStorage');
+    return false;
+  }
+  
+}
+
+htmlConstructor();
+// Intentar cargar desde localStorage primero, si no hay datos, hacer fetch
+if(!loadCharFromStorage()){
+    fetchCharacter();
+}
+
+
 
 //Id, name, img
